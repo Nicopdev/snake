ed8fcb72811c360836c11907c0ca0f5891a278a5
Nico <46602571+Nicopdev@users.noreply.github.com>
2020-11-08 13:07:10 +0100

    Update snake.cpp

--- cpp_version/snake.cpp
+++ cpp_version/snake.cpp
@@ -1,9 +1,9 @@
-
-
-
-
-
-
+//
+//  main.cpp
+//  line
+//
+//  Created by Nicolò Padovan on 07/11/2020.
+//
 
 #include <iostream>
 #include <ncurses.h>
@@ -12,17 +12,18 @@
 #include <deque>
 
 using namespace std;
+using Point = std::array<short, 2>;
 
 class Snake {
     public:
-        int w, h;
-        unsigned int score = 1;
-        array<short int, 2> currentPosition = {};
-        array<short int, 2> foodPosition = {};
-        std::deque<array<short int, 2>> history;
+        short w, h;
+        std::deque<Point> history;
+        unsigned int score = 0;
+        array<short, 2> currentPosition = {10, 10};
+        array<short, 2> foodPosition = {};
         unsigned char direction = 0; // 0 = UP, 1 = RIGHT, 2 = DOWN, 3 = LEFT
-        unsigned long int startTime;
-        int edges[2][2] = {{0,0},{0,0}};
+        time_t startTime = 0;
+        short edges[2][2] = {{0,0},{0,0}};
         bool gameOver = FALSE;
         void addString(const char text[2], int y, int x);
         void start();
@@ -37,32 +38,32 @@ class Snake {
 Snake::Snake() {
     curs_set(0);
     updateDims();
-    
+
     if (Snake::w%2 != 0) {
         resizeterm(Snake::h, Snake::w-1);
         updateDims();
     }
     
-    Snake::edges[0][1] = w;
-    Snake::edges[1][1] = h;
-    
+    edges[0][1] = w;
+    edges[1][1] = h;
+
     reset();
     start();
 }
 
 void Snake::reset() {
-    startTime = clock();
+    startTime = time(0);
     gameOver = FALSE;
     score = 1;
+    timeout(0);
     
     while (!history.empty()) {
         history.erase(history.begin());
     }
-    currentPosition[0] = Snake::w/2; currentPosition[1] = h/2;
+    currentPosition[0] = w/2; currentPosition[1] = h/2;
     if (currentPosition[0]%2 != 0) {
         currentPosition[0] += 1;
     }
-    timeout(1);
     direction = 0;
     foodPosition[0] = 0; foodPosition[1] = 0;
     generateFood(TRUE);
@@ -77,22 +78,22 @@ void Snake::start() {
         while (!gameOver) {
             updateWindow();
         }
-        
+
         timeout(-1);
-        clear();
-        wmove(stdscr, 0, 0);
-        wprintw(stdscr, "Game-over");
+//        clear();
+//        wmove(stdscr, 0, 0);
+//        wprintw(stdscr, "Game-over");
         getch();
         reset();
     }
 }
 
 void Snake::updateWindow() {
-    unsigned long currentTime = clock() - startTime / CLOCKS_PER_SEC;
+    time_t currentTime = difftime(time(0), startTime);
     
-    if (currentTime > 1) {
+    if (currentTime > 0.1) {
         int key = getch();
-        
+
         if (key == KEY_UP) {
             if (direction != 2) {
                 direction = 0;
@@ -110,7 +111,7 @@ void Snake::updateWindow() {
                 direction = 3;
             }
         }
-        
+
         history.push_front(currentPosition);
         while (history.size() > score) {
             history.erase(history.end());
@@ -131,18 +132,18 @@ void Snake::updateWindow() {
                 break;
         }
         
-        clear();
-        array<short int, 2> foodPos = generateFood(FALSE);
-        addString("FF", foodPos[1], foodPos[0]);
-        addString("AA", currentPosition[1], currentPosition[0]);
-        
-        bool found = find(history.begin(), history.end(), currentPosition) == history.end();
-        if (currentPosition[0] < edges[0][0] || currentPosition[0] > edges[0][1] || currentPosition[1] < edges[0][1] || currentPosition[1] > edges[1][1] || found) {
+        bool find = std::find(history.begin(), history.end(), currentPosition) != history.end();
+        if (currentPosition[0] < edges[0][0] || currentPosition[0] > edges[0][1] || currentPosition[1] < edges[1][0] || currentPosition[1] > edges[1][1] || find) {
             gameOver = TRUE;
             return;
+        } else {
+            clear();
+            array<short int, 2> foodPos = generateFood(FALSE);
+            addString("FF", foodPos[1], foodPos[0]);
+            addString("AA", currentPosition[1], currentPosition[0]);
+            generateTail();
         }
-        
-        startTime = clock();
+        startTime = time(0);
     }
 }
 
@@ -151,12 +152,12 @@ array<short int, 2> Snake::generateFood(bool ignore) {
         score += 1;
         array<short int, 2> newFoodPos;
         while (TRUE) {
-            short int rand_1 = rand()%edges[0][1];
-            short int rand_2 = rand()%edges[1][1];
-            
-            newFoodPos = {rand_1, rand_2};
-//            newFoodPos[0] = rand_1; newFoodPos[1] = rand_2;
-            
+            short rand_1 = rand()%edges[0][1];
+            short rand_2 = rand()%edges[1][1];
+
+//            newFoodPos = {rand_1, rand_2};
+            newFoodPos[0] = rand_1; newFoodPos[1] = rand_2;
+
             if (newFoodPos != currentPosition && newFoodPos[0]%2==0) {
                 break;
             }
@@ -170,8 +171,7 @@ array<short int, 2> Snake::generateFood(bool ignore) {
 
 void Snake::generateTail() {
     for(int i=0; i<history.size(); i++) {
-        array<short int, 2> position = history[i];
-        addString("AA", position[1], position[0]);
+        addString("AA", history[i][1], history[i][0]);
     }
 }
 
@@ -180,10 +180,35 @@ void Snake::addString(const char text[2], int y, int x) {
     addstr(text);
 }
 
+class Player {
+public:
+    array<short, 2> currentPosition;
+    short direction = 0;
+    short h, w;
+    Player();
+    void updateWindow();
+};
+
+Player::Player() {
+    getmaxyx(stdscr, h, w);
+    
+}
+
+void Player::updateWindow() {
+    
+}
+
+
 int main() {
+    
     initscr();
+    keypad(stdscr, TRUE);
     start_color();
     curs_set(0);
+    cbreak();
+    noecho();
+//    nodelay(stdscr, TRUE);
     
     Snake snake;
 }
+
